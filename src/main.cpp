6972b129d0fe77cb5e904607de6c25f85a7503e6
dmitriyBreyfogel <breyfogeld@bk.ru>
2026-09-15 22:55:25 +0300

    [CHORE]: Add lexer entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,23 @@
-int main()
+#include <fstream>
+#include <iostream>
+
+#include <FlexLexer.h>
+
+int main(int argc, char* argv[])
 {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <file>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream input(argv[1]);
+    if (!input.is_open()) {
+        std::cerr << "Unable to open file: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    yyFlexLexer lexer(&input, &std::cout);
+    lexer.yylex();
+
     return 0;
 }
